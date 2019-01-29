df63bbd88faf3e57b136a1a3557d5ef598d8cc1b
ana <ana@lynx.nz>
2019-01-30 11:52:45 +1300

    fix config error

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,10 @@
-import { configure } from '@storybook/react';
+import { configure } from '@storybook/react'
 import '../src/index.css'
 
-const req = require.context('../src', true, /.stories.js$/)
+const req = require.context('../src', true, /\.stories\.js$/)
 
 function loadStories() {
-  req.keys().forEach(fileName => req(filename))
+  req.keys().forEach(req)
 }
 
-configure(loadStories, module);
+configure(loadStories, module)
